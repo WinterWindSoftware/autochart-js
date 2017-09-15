f9225c9c7e99a393b6221a52aefa18b2b797bd91
Paul Swail <paul@winterwindsoftware.com>
2017-09-15 13:33:39 +0100

    Move replay logic into index.js and fix module export

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-import './src/tracker';
+import Tracker from './src/tracker';
+import Utils from './src/utils';
+
+const autochart = new Tracker();
+
+// Replay queued async methods
+if (window) {
+    // Copy queued methods
+    const queued = window.autochart || [];
+
+    // Replace stubbed autochart global with real singleton instance + utils instance
+    window.autochart = autochart;
+    window.autochart.util = Utils;
+
+    // Replay any queued methods
+    while (queued.length > 0) {
+        const args = queued.shift();
+        const method = args.shift();
+        if (autochart[method]) {
+            autochart[method](...args);
+        }
+    }
+
+    // Export tracker library for testing
+    window.AutoChartTracker = Tracker;
+}
+
+export default autochart;
--- src/tracker.js
+++ src/tracker.js
@@ -328,28 +328,4 @@ Tracker.prototype.callback = function callback(fn) {
     return this;
 };
 
-// ============================================================================================
-// Export autochart global singleton and replay queued async methods
-// ============================================================================================
-const autochart = new Tracker();
-module.exports = autochart;
-if (window) {
-    // Copy queued methods
-    const queued = window.autochart || [];
-
-    // Replace stubbed autochart global with real singleton instance + utils instance
-    window.autochart = autochart;
-    window.autochart.util = Utils;
-
-    // Replay any queued methods
-    while (queued.length > 0) {
-        const args = queued.shift();
-        const method = args.shift();
-        if (autochart[method]) {
-            autochart[method](...args);
-        }
-    }
-
-    // Export tracker library for testing
-    window.AutoChartTracker = Tracker;
-}
+export default Tracker;
